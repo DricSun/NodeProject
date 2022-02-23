bd9649b08dcd6edfd835a1b365accaec60c827ed
Thibaut <thibaut.francois@edu.devinci.fr>
2022-02-23 08:33:20 +0100

    add stuf

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.use((req, res) => {
 });
 
 
+app.use((req,res,next)=> {
+    res.setHeader('Access-Control-Allow-Origin','*')
+    res.setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content,Accept,Content-Type,Authorization')
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,DELETE')
+    next()
+})
+
 //  YO
 module.exports = app;
 
--- modele/modele.js
+++ modele/modele.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 
 const postShema = mongoose.Schema({
-    titre:{type:String, require:true}
-    text:{type:String, require:true}
-    autor:{type:String, require:true}
+    titre:{type:String, require:true},
+    text:{type:String, require:true},
+    autor:{type:String, require:true},
     urlImage:{type:String, require:false}
 })
 
